b1ac03851c9d2025c1e34c4b161cf28efb80752d
steven hunt <st3v3n.hunt@gmail.com>
2017-06-30 15:24:53 +0100

    :heartpulse: Create individual css files

--- brunch-config.js
+++ brunch-config.js
@@ -1,6 +1,14 @@
 module.exports = {
   files: {
-    stylesheets: { joinTo: 'nhsuk.css' },
+    stylesheets: {
+      joinTo: {
+        'nhsuk.css': /nhsuk.scss/,
+        'nhsuk-ie6.css': /nhsuk-ie6.scss/,
+        'nhsuk-ie7.css': /nhsuk-ie7.scss/,
+        'nhsuk-ie8.css': /nhsuk-ie8.scss/,
+        'nhsuk-print.css': /nhsuk-print.scss/
+      }
+    },
   },
   plugins: {
     sass: {

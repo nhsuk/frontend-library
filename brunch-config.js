9e2a4f6bba9d9a9cc0aa1431d7ad5e31fff2778e
steven hunt <st3v3n.hunt@gmail.com>
2018-03-15 17:27:03 +0000

    :up: Upgrade to eslint-config-nhsuk@0.13.0 with fixes

--- brunch-config.js
+++ brunch-config.js
@@ -6,11 +6,11 @@ module.exports = {
         'nhsuk-ie6.css': /nhsuk-ie6.scss/,
         'nhsuk-ie7.css': /nhsuk-ie7.scss/,
         'nhsuk-ie8.css': /nhsuk-ie8.scss/,
-        'nhsuk-print.css': /nhsuk-print.scss/
-      }
+        'nhsuk-print.css': /nhsuk-print.scss/,
+      },
     },
   },
   plugins: {
-    sass: {}
-  }
+    sass: {},
+  },
 };
